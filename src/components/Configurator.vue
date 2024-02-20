09a7df03598070f29048715aa43c6ccf2dc5c9c2
habdevs <habdevs@yandex.ru>
2024-02-20 20:40:53 +0500

    fix: overflow

--- src/components/Configurator.vue
+++ src/components/Configurator.vue
@@ -2,46 +2,23 @@
   <div class="configurator-wrapper">
     <v-card-text class="configurator-size">
       <label>Размер устройства:</label>
-      <v-select
-        rounded="lg"
-        v-model="size"
-        :items="drinkOptions.map(option => option.size)"
-        label="Выберите размер"
-      ></v-select>
+      <v-select rounded="lg" v-model="size" :items="drinkOptions.map(option => option.size)"
+        label="Выберите размер"></v-select>
 
-      <v-img
-        v-if="size === 'standard'"
-        src="@/assets/standard-coffee-machine.png"
-        alt="Стандартная кофемашина"
-        height="100"
-        width="100"
-      ></v-img>
+      <v-img v-if="size === 'standard'" src="@/assets/standard-coffee-machine.png" alt="Стандартная кофемашина"
+        height="100" width="100"></v-img>
 
-      <v-img
-        v-else-if="size === 'large'"
-        src="@/assets/large-coffee-machine.png"
-        alt="Увеличенная кофемашина"
-        height="100"
-        width="100"
-      ></v-img>
-      <v-img
-        v-else-if="size === 'veryLarge'"
-        src="@/assets/very-coffee-machine.png"
-        alt="Огромная кофемашина"
-        height="100"
-        width="100"
-      ></v-img>
+      <v-img v-else-if="size === 'large'" src="@/assets/large-coffee-machine.png" alt="Увеличенная кофемашина"
+        height="100" width="100"></v-img>
+      <v-img v-else-if="size === 'veryLarge'" src="@/assets/very-coffee-machine.png" alt="Огромная кофемашина"
+        height="100" width="100"></v-img>
     </v-card-text>
 
 
     <v-card-text class="configurator-drink-count">
       <label>Количество напитков:</label>
-      <v-select
-        rounded="lg"
-        v-model="drinkCount"
-        :items="drinkOptions.map(option => option.drinkCount)"
-        label="Выберите количество"
-      ></v-select>
+      <v-select rounded="lg" v-model="drinkCount" :items="drinkOptions.map(option => option.drinkCount)"
+        label="Выберите количество"></v-select>
     </v-card-text>
     <v-btn @click="addToStorage">Добавить в хранилище</v-btn>
     <h2>Выбранные опции:</h2>
@@ -98,7 +75,7 @@ const addToStorage = () => {
   coffeeMachineStore.setSize(size.value);
   coffeeMachineStore.setDrinkCount(drinkCount.value || 0);
   console.log(`добавлено: ${size.value} and ${drinkCount.value}`);
-  
+
   coffeeMachineStore.addToStorage({
     size: size.value,
     drinkCount: drinkCount.value || 0,
@@ -113,8 +90,8 @@ const addToStorage = () => {
   flex-direction: column;
   align-items: center;
   margin: 0 auto;
-  overflow-x: scroll;
 }
+
 .configurator-size,
 .configurator-drink-count {
   width: 300px;
